8a5c89b41477179c7dfaec3bc3de4ed5ff2e4de2
CESAR BR <cesarbr@192.168.1.41>
2026-06-13 14:05:17 -0600

    cambios

---
+++ app/(protected)/caja/page.tsx
@@ -0,0 +1,8 @@
+export default async function Page() {
+    // Safe to perform server-side data fetching directly here
+    return (
+        <main>
+            <h1>Welcome to Next.js</h1>
+        </main>
+    );
+}
